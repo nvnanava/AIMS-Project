8a9a99c51a7eba1bb2073eb012b6664925912e31
chillouthanna <145229679+HannaKaur@users.noreply.github.com>
2025-03-26 11:32:47 -0700

    Scrum-37: Implement Asset list with clickable rows

---
+++ Pages/AssetList.cshtml.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using AssetTrackingSystem.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AssetTrackingSystem.Pages
+{
+    public class AssetListModel : PageModel
+    {
+        private readonly AssetDbContext _db;
+
+        public AssetListModel(AssetDbContext db) => _db = db;
+
+        public IList<Asset> Assets { get; set; } = new List<Asset>();
+
+        public async Task OnGetAsync()
+        {
+            Assets = await _db.Assets.ToListAsync();
+        }
+    }
+}
+
