290884368ab754211573cb3594090221f0ad589b
CRLanzaro <cameronlanzaro@csus.edu>
2025-03-25 21:24:15 -0700

    Scaffolded EF Core models for Hardware and Software tables with AimsDbContext in Data folder.

---
+++ AIMS/Data/AimsDbContext.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using AIMS.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AIMS.Data;
+
+public partial class AimsDbContext : DbContext
+{
+    public AimsDbContext()
+    {
+    }
+
+    public AimsDbContext(DbContextOptions<AimsDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Hardware> Hardwares { get; set; }
+
+    public virtual DbSet<Software> Softwares { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Connection String"); // Replace "Connection String" with your connection string. Will put format in discord.
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Hardware>(entity =>
+        {
+            entity.HasKey(e => e.AssetTag).HasName("PK__Hardware__89F276AACC2EBD54");
+
+            entity.ToTable("Hardware");
+
+            entity.HasIndex(e => e.SerialNumber, "UQ__Hardware__048A000838FBE506").IsUnique();
+
+            entity.Property(e => e.AssetName).HasMaxLength(255);
+            entity.Property(e => e.AssetType).HasMaxLength(255);
+            entity.Property(e => e.Manufacturer).HasMaxLength(255);
+            entity.Property(e => e.Model).HasMaxLength(255);
+            entity.Property(e => e.SerialNumber).HasMaxLength(255);
+            entity.Property(e => e.Status).HasMaxLength(255);
+        });
+
+        modelBuilder.Entity<Software>(entity =>
+        {
+            entity.HasKey(e => e.SoftwareId).HasName("PK__Software__25EDB8DC7B909065");
+
+            entity.ToTable("Software");
+
+            entity.HasIndex(e => e.SoftwareLicenseKey, "UQ__Software__953EDCAE2A3AE925").IsUnique();
+
+            entity.Property(e => e.SoftwareId).HasColumnName("SoftwareID");
+            entity.Property(e => e.SoftwareCost).HasColumnType("decimal(10, 2)");
+            entity.Property(e => e.SoftwareDeploymentLocation).HasMaxLength(255);
+            entity.Property(e => e.SoftwareLicenseKey).HasMaxLength(255);
+            entity.Property(e => e.SoftwareName).HasMaxLength(255);
+            entity.Property(e => e.SoftwareType).HasMaxLength(255);
+            entity.Property(e => e.SoftwareVersion).HasMaxLength(255);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
--- AIMS/Models/Hardware.cs
+++ AIMS/Models/Hardware.cs
@@ -1,30 +1,25 @@
-
-using Microsoft.EntityFrameworkCore; //team needs to get entity framework packages
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace AIMS.Models;
 
-//Base class for Hardware without functionality. Subclasses underneath to show inheritance example.
-//Interacts with the db through AssetContext.cs in the Data folder.
-
-public class Hardware
+public partial class Hardware
 {
-    public int Id { get; set; }
-    public int TagNumber { get; set; }
-    public string? Status { get; set; }
-    public string? Type { get; set; }
-    public string? AssignedTo { get; set; }
-}
+    public int AssetTag { get; set; }
 
-// Inherits from Hardware
-public class Laptop : Hardware
-{
-    public bool HasTouchScreen { get; set; }
-}
+    public string AssetName { get; set; } = null!;
 
-// Inherits from Hardware
-public class Desktop :  Hardware
-{
-    public int MonitorsAssigned {get; set;}
+    public string AssetType { get; set; } = null!;
+
+    public string Status { get; set; } = null!;
+
+    public string Manufacturer { get; set; } = null!;
+
+    public string Model { get; set; } = null!;
+
+    public string SerialNumber { get; set; } = null!;
+
+    public DateOnly WarrantyExpiration { get; set; }
+
+    public DateOnly PurchaseDate { get; set; }
 }
