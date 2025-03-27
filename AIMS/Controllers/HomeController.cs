0cebdd14cb66249327bb89af0c92bed11cd15b0e
norinphlong <norinphlog@gmail.com>
2025-03-26 23:37:05 -0700

    Backend search and sort functionality in HomeController

--- AIMS/Controllers/HomeController.cs
+++ AIMS/Controllers/HomeController.cs
@@ -1,21 +1,36 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using AIMS.Models;
+using System.Linq;
 
 namespace AIMS.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly AIMSContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, AIMSContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
-    public IActionResult Index()
+    public IActionResult Index(string search, string sortBy)
     {
-        return View();
+        var assets = _context.Assets.AsQueryable();
+
+        if (!string.IsNullOrEmpty(search)) {
+            assets = assets.Where(a => a.Name.Contains(search) || a.Type.Contains(search) || a.Status.Contains(search) || (a.AssignedTo ?? "").Contains(search));
+        }
+        assets = sortBy?.ToLower() switch {
+            "name" => assets.OrderBy(a => a.Name),
+            "type" => assets.OrderBy(a => a.Type),
+            "status" => assets.OrderBy(a => a.Status),
+            "assigned" => assets.OrderBy(a => a.AssignedTo),
+            _=> assets
+        };
+        return View(assets.ToList());
     }
 
     public IActionResult Privacy()
