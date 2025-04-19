64323b633e101b3a5d9f4f2950ee7cc26ceffbbf
norinphlong <norinphlog@gmail.com>
2025-04-19 11:02:52 -0700

    SCRUM-119: Feedback Form in Navbar

--- AIMS/Controllers/HomeController.cs
+++ AIMS/Controllers/HomeController.cs
@@ -35,6 +35,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Feedback()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
