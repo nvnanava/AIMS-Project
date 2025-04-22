bd47b64b0d1ba036501fd42ce0240f6657bc1994
CRLanzaro <cameronlanzaro@csus.edu>
2025-04-22 14:55:02 -0700

    Implemented functionality to add asset to db using the AssetAddForm

--- schemas/hardware.sql
+++ schemas/hardware.sql
@@ -1,11 +1,11 @@
 CREATE TABLE Hardware (
-    AssetTag int PRIMARY KEY IDENTITY(1,1),
+    AssetTag INT PRIMARY KEY,
     AssetName NVARCHAR(255) NOT NULL,
     AssetType NVARCHAR(255) NOT NULL,
     Status NVARCHAR(255) NOT NULL,
     Manufacturer NVARCHAR(255) NOT NULL,
     Model NVARCHAR(255) NOT NULL,
     SerialNumber NVARCHAR(255) UNIQUE NOT NULL,
-    WarrantyExpiration date NOT NULL,
-    PurchaseDate date NOT NULL
-)
+    WarrantyExpiration DATE NOT NULL,
+    PurchaseDate DATE NOT NULL
+);
