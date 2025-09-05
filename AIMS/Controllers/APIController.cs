3ec7c2af1e13d3151b2abd4dc1d0c3f6a06d6cb2
akalustat <akalustat.singh@gmail.com>
2025-09-05 13:45:07 -0700

    [SCRUM 122] feat: Add initial CreateAssignment and GetAssignment functions

---
+++ AIMS/Controllers/APIController.cs
@@ -0,0 +1,97 @@
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AIMS.Data;
+using AIMS.Models;
+
+namespace AIMS.Controllers;
+
+[ApiController]
+[Route("api/")]
+public class ApiController : ControllerBase
+{
+    private readonly AimsDbContext _db;
+    public ApiController(AimsDbContext db) => _db = db;
+
+    // Quick sanity: table counts
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Assignment([FromBody] CreateAssignmentDto req)
+    {
+
+        // short-circuit: reject if user not authenticated
+
+        // make sure we get one of the indentifiers for an asset
+        if (req.AssetTag == null && req.SoftwareID == null)
+        {
+            // for correct methods corresponding to request messages, see the Methods section under https://learn.microsoft.com/en-us/dotnet/api/system.web.http.apicontroller?view=aspnetcore-2.2
+            return BadRequest("You must specify either AssetTag (HardwareID) or SoftwareID.");
+        }
+
+        // validate that AssetTag exists if specified
+        var assetTagExists = await _db.HardwareAssets.AnyAsync(hw => hw.HardwareID == req.AssetTag);
+        if (req.AssetTag != null && !assetTagExists)
+        {
+            return BadRequest("Please specify a valid AssetTag");
+        }
+
+        // validate that SoftwareID exists if specified
+        var softwareIDExists = await _db.SoftwareAssets.AnyAsync(sw => sw.SoftwareID == req.SoftwareID);
+        if (req.SoftwareID != null && !softwareIDExists)
+        {
+            return BadRequest("Please specify a valid SoftwareID");
+        }
+
+        Assignment newAssignment = new Assignment
+        {
+            SoftwareID = req.SoftwareID,
+            AssetKind = req.AssetKind,
+            AssetTag = req.AssetTag,
+            UserID = req.UserID
+        };
+
+        // finally, create assignment
+            _db.Assignments.Add(newAssignment);
+        await _db.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetAssignment), new {AssignmentID = newAssignment.AssignmentID}, req);
+    }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+    public async Task<IActionResult> GetAssignment(int AssignmentID)
+    {
+        var assignment = await _db.Assignments.Where(a => a.AssignmentID == AssignmentID).FirstOrDefaultAsync();
+        if (assignment == null)
+        {
+            return NotFound("Please specify a valid AssignmentID");
+        }
+
+        return Ok(assignment);
+    }
+
+}
+
+/**
+
+See the following link for the reason for why we use DTOs:
+https://blog.devart.com/working-with-data-transfer-objects-in-asp-net-core.html
+
+We might move this to a new file: https://learn.microsoft.com/en-us/aspnet/web-api/overview/data/using-web-api-with-entity-framework/part-5
+
+(TODO): We can use an automapper to generate the bindings from this DTO to the actual model: https://automapper.io
+*/
+public class CreateAssignmentDto
+{
+    public int UserID { get; set; }
+
+    // What (one of these must be set, enforced in code/migration)
+    public AssetKind AssetKind { get; set; }
+    public int? AssetTag { get; set; }      // when Hardware
+    public int? SoftwareID { get; set; }    // when Software
+
+}
