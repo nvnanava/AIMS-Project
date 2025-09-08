91b8c3fd1adbf19c65c573c0de750a77d71b5b83
Niyant Nanavati <niyant397@gmail.com>
2025-09-07 20:50:30 -0700

    Task 1: Set Up Role-Based Authorization in Backend SCRUM 148-151

--- AIMS/Program.cs
+++ AIMS/Program.cs
@@ -1,5 +1,9 @@
 using AIMS.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,8 +24,61 @@ builder.Services.AddDbContext<AimsDbContext>(options =>
     options.UseSqlServer(cs);
 });
 
+
 // Optional feature flag for prod seeding (defaults false)
 var allowProdSeed = builder.Configuration.GetValue<bool>("AllowProdSeed", false);
+
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme) // Configure the app to use OpenID Connect authentication with Azure AD
+//.AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd")); // Bind the Azure AD settings from configuration to the authentication options
+.AddMicrosoftIdentityWebApp(options =>
+{
+    builder.Configuration.Bind("AzureAd", options);
+    options.AccessDeniedPath = "/Home/Error"; // Redirects users to a custom error page if they are denied access
+    options.TokenValidationParameters.RoleClaimType = "roles"; // This  maps the "roles" claim from the token to the user's roles in the application
+});
+
+builder.Services.AddAuthorizationBuilder()
+  .AddPolicy("mbcAdmin", policy => // Only users with the "Admin" role can access any action methods in this controller
+    policy.RequireAssertion(context =>
+        context.User.HasClaim(c =>
+            c.Type == "preferred_username" &&
+            new[] {
+                // our test accounts for now, you should be able to login with your csus accounts.
+                "niyant397@gmail.com",
+                "nvnanavati@csus.edu",
+                "akalustatsingh@csus.edu",
+                "tburguillos@csus.edu",
+                "keeratkhandpur@csus.edu",
+                "suhailnajimudeen@csus.edu",
+                "hkaur@csus.edu",
+                "cameronlanzaro@csus.edu",
+                "norinphlong@csus.edu"
+
+
+            }.Contains(c.Value))))
+
+  .AddPolicy("mbcHelpDesk", policy => //Help Desk Users, dummy names for now but you can add your email here to test.
+    policy.RequireAssertion(context =>
+        context.User.HasClaim(c =>
+            c.Type == "preferred_username" &&
+            new[] {
+                "barryAllen@centralcity.edu"
+
+            }.Contains(c.Value))))
+
+  .AddPolicy("mbcSupervisor", policy =>
+    policy.RequireAssertion(context =>
+        context.User.HasClaim(c =>
+            c.Type == "preferred_username" &&
+            new[] {
+                "richardGrayson@gotham.edu"
+            }.Contains(c.Value))));
+
+
+
+builder.Services.AddRazorPages() // Add support for Razor Pages and integrate Microsoft Identity UI components
+    .AddMicrosoftIdentityUI(); // Adds Razor UI pages for authentication and user management
+
 
 var app = builder.Build();
 
@@ -46,15 +103,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(); //used for testing APIs. Swagger UI will be available at /swagger/index.html
 }
 else
-{ 
+{
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
     app.UseHttpsRedirection();
+    app.UseRouting();
 }
 
-app.UseRouting();
 
+app.UseAuthentication(); // Enable Azure authentication middleware
 app.UseAuthorization();
 
 app.MapStaticAssets();
