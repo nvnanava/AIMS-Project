be35f9035703ba35d627049e9d836af6bce628a2
Keerat Khandpur <iamkeeratk@gmail.com>
2025-09-14 15:47:48 -0700

    SCRUM-127

--- AIMS/Controllers/SoftwareController.cs
+++ AIMS/Controllers/SoftwareController.cs
@@ -29,6 +29,62 @@ public class SoftwareController : ControllerBase
         var users = await _softwareQuery.GetAllSoftwareAsync();
         return Ok(users);
     }
-   
+
+    [HttpPost("add")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> AddSoftware([FromBody] CreateSoftwareDto dto)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        // Map DTO → Entity
+        var software = new Software
+        {
+            SoftwareName = dto.SoftwareName,
+            SoftwareType = dto.SoftwareType,
+            SoftwareVersion = dto.SoftwareVersion,
+            SoftwareLicenseKey = dto.SoftwareLicenseKey,
+            SoftwareLicenseExpiration = dto.SoftwareLicenseExpiration,
+            SoftwareUsageData = dto.SoftwareUsageData,
+            SoftwareCost = dto.SoftwareCost
+        };
+
+        _db.SoftwareAssets.Add(software);
+        await _db.SaveChangesAsync();
+
+        return CreatedAtAction(
+            nameof(GetAllSoftware),   // could also make a GetById and reference it here
+            new { id = software.SoftwareID },
+            software
+        );
+    }
+
+    [HttpPut("edit/{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> EditSoftware(int id, [FromBody] UpdateSoftwareDto dto)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        var software = await _db.SoftwareAssets.FindAsync(id);
+        if (software == null)
+            return NotFound();
+
+        // Update fields
+        software.SoftwareName = dto.SoftwareName;
+        software.SoftwareType = dto.SoftwareType;
+        software.SoftwareVersion = dto.SoftwareVersion;
+        software.SoftwareLicenseKey = dto.SoftwareLicenseKey;
+        software.SoftwareLicenseExpiration = dto.SoftwareLicenseExpiration;
+        software.SoftwareUsageData = dto.SoftwareUsageData;
+        software.SoftwareCost = dto.SoftwareCost;
+
+        await _db.SaveChangesAsync();
+
+        return Ok(software);
+    }
 
 }
