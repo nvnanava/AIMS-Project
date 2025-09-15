0682304c31921da4c2b3a548ff05199aa1a8c950
Akal-Ustat Singh <akalustat.singh@gmail.com>
2025-09-15 08:09:26 -0700

    SCRUM 122 - Subtask 4 (SCRUM 147) Audit Logging (#54)
    * feat: Create AuditLogs when assigning an asset
    
    * refactor: change get-all to list route
    
    * Task 1: Set Up Role-Based Authorization in Backend SCRUM 148-151
    
    * Task 1: Set Up Role-Based Authorization in Backend SCRUM 148-151
    
    * Task 1: Set Up Role-Based Authorization in Backend SCRUM 148-151
    
    * Restore scoped AuditLogQuery
    
    * fix: AuditLog get methods not showing correct AssetTag and AssetKind
    
    * Remove console.writeline
    
    ---------
    
    Co-authored-by: Niyant Nanavati <niyant397@gmail.com>

--- AIMS/Controllers/AuditLogController.cs
+++ AIMS/Controllers/AuditLogController.cs
@@ -1,12 +1,60 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using AIMS.Data;
+using AIMS.Models;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+namespace AIMS.Controllers;
 
-namespace AIMS.Controllers
+
+
+[ApiController]
+[Route("api/audit")]
+public class AuditLogController : Controller
 {
-    public class AuditLogController : Controller
+    private readonly AimsDbContext _db;
+    private readonly AuditLogQuery _auditQuery;
+    public AuditLogController(AimsDbContext db, AuditLogQuery auditQuery)
     {
-        public IActionResult Index()
+        _db = db;
+        _auditQuery = auditQuery;
+
+    }
+
+    [HttpPost("create")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> CreateRecord([FromQuery] CreateAuditRecordDto req)
+    {
+
+        try
         {
-            return View();
+
+            int newRecordID = await _auditQuery.createAuditRecordAsync(req);
+            return CreatedAtAction(nameof(GetAuditRecord), new { AuditRecordID = newRecordID }, req);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
         }
     }
+
+    [HttpGet("get")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetAuditRecord([FromQuery] int AuditRecordID)
+    {
+        var record = await _auditQuery.GetAuditRecordAsync(AuditRecordID);
+        if (record is null) return NotFound();
+        return Ok(record);
+    }
+    
+    [HttpGet("list")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAllRecords()
+    {
+        return Ok(await _auditQuery.GetAllAuditRecordsAsync());
+    }
 }
+
