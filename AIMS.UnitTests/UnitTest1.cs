76b4078ce786984d7822ddd86469a35af74ac862
akalustat <akalustat.singh@gmail.com>
2025-09-25 12:51:45 -0700

    Create Unit Tests Project

---
+++ AIMS.UnitTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿namespace AIMS.UnitTests;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AIMS.Data;
+using AIMS.Queries;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
