192c8520918faa3bb26797b6dd08c822bb5e600d
ERROR-Does-Not-Compute <91012242+ERROR-Does-Not-Compute@users.noreply.github.com>
2025-11-09 21:58:46 -0800

    chore(auth+tests): unify Entra + TestAuth flow and stabilize full E2E suite (v2)

--- AIMS/Program.cs
+++ AIMS/Program.cs
@@ -11,6 +11,7 @@ using AIMS.Utilities; // TestAuthHandler
 using Azure.Identity;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Mvc;
